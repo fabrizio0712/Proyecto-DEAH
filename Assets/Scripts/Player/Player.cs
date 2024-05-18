4f92c2b9c35875a2ea927cf1ee7847508ee1fd45
Fabrizio <69770999+fabrizio0712@users.noreply.github.com>
2024-05-18 20:18:32 -0300

    Logica de disparo + Logica de CaC + Logica de municion + correcciones

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,9 +1,7 @@
-using JetBrains.Annotations;
+
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
@@ -15,18 +13,22 @@ public class Player : MonoBehaviour
 
     //Componentes
     Rigidbody2D Rb2D;
-    SpriteRenderer spriteRenderer;
-    public GameObject MeleeAttackArea;
+    public Transform MeleeAttackArea;
 
     //Banderas
     public bool canDoubleJump;
     public bool canMove;
-    public bool dashing;
-    public bool attacking;
+    public bool canDash;
+    public bool Dashing;
+    public bool Attacking;
+    public bool Shooting;
+    public bool facingRignt;
 
     //Cooldowns
     public float dashCooldown = 0.5f;
     public float currentDashCooldown = 0;
+    public float shootCooldown = 0.5f;
+    public float currentShootCooldown = 0;
 
     //Duracion
     public float dashDuration = 0.2f;
@@ -34,61 +36,139 @@ public class Player : MonoBehaviour
     public float meleeAttackDuration = 0.2f;
     public float currentMeleeAttackDuration = 0;
 
+    //Ammo
+    public float basicAmmoCount = 5;
+    public float blastAmmoCount = 5;
+    public float toxicAmmoCount = 5;
+    public BulletTipe currentBulletTipe = BulletTipe.Basic;
+
     private void Awake()
     {
         Rb2D = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         canMove = true;
+        facingRignt = true;
     }
 
     private void Update()
     {
         AttackLogic();
-        HorizontalMoveLogic();
-        JumpLogic();
-        DoubleJumpLogic();
+        ChangeAmmo();
         CustomGravityLogic();
         DashLogic();
+        DoubleJumpLogic();
+        HorizontalMoveLogic();
+        JumpLogic();
+        ShootLogic();
+    }
+    void ChangeAmmo() 
+    {
+        if (Input.GetKeyDown("1") && currentBulletTipe != BulletTipe.Basic)
+        {
+            currentBulletTipe = BulletTipe.Basic;
+            Debug.Log("Current Ammo Tipe is Basic");
+        } 
+        else if (Input.GetKeyDown("2") && currentBulletTipe != BulletTipe.Blast)
+        {
+            currentBulletTipe = BulletTipe.Blast;
+            Debug.Log("Current Ammo Tipe is Blast");
+        } 
+        else if (Input.GetKeyDown("3") && currentBulletTipe != BulletTipe.Toxic)
+        {
+            currentBulletTipe = BulletTipe.Toxic;
+            Debug.Log("Current Ammo Tipe is Toxic");
+        }
+    }
+    void ShootLogic() 
+    {
+        if (canMove) 
+        {
+            if (Input.GetKey("c") && !Attacking && !Dashing) 
+            {
+                if (currentShootCooldown <= 0) 
+                {
+                    switch (currentBulletTipe) 
+                    {
+                        case BulletTipe.Basic:
+                            if (basicAmmoCount > 0) 
+                            {
+                                Debug.Log("pium Basic");
+                                basicAmmoCount--;
+                                Debug.Log("Ammo Remaining: " + basicAmmoCount);
+                            }
+                            break;
+                        case BulletTipe.Blast:
+                            if (blastAmmoCount > 0)
+                            {
+                                Debug.Log("pium Blast");
+                                blastAmmoCount--;
+                                Debug.Log("Ammo Remaining: " + blastAmmoCount);
+                            }
+                            break;
+                        case BulletTipe.Toxic:
+                            if (toxicAmmoCount > 0)
+                            {
+                                Debug.Log("pium Toxic");
+                                toxicAmmoCount--;
+                                Debug.Log("Ammo Remaining: " + toxicAmmoCount);
+                            }
+                            break;
+                    }
+                    currentShootCooldown = shootCooldown;
+                }
+            }
+        }
+        if (currentShootCooldown > 0)
+        {
+            currentShootCooldown -= Time.deltaTime;
+        }
     }
     void AttackLogic() 
     {
         if (canMove) 
         {
-            if (Input.GetKeyDown("x") && !attacking)
+            if (Input.GetKeyDown("x") && !Attacking && !Dashing)
             {
-                attacking = true;
+                Attacking = true;
                 currentMeleeAttackDuration = meleeAttackDuration;
-                MeleeAttackArea.SetActive(true);
+                MeleeAttackArea.gameObject.SetActive(true);
             }
-        }
-        if (currentMeleeAttackDuration > 0)
-        {
-            currentMeleeAttackDuration -= Time.deltaTime;
-        }
-        else 
-        {
-            attacking = false;
-            MeleeAttackArea.SetActive(false);
+            if (Attacking) 
+            {
+                if (currentMeleeAttackDuration > 0) 
+                {
+                    currentMeleeAttackDuration -= Time.deltaTime;
+                }
+                else 
+                {
+                    Attacking = false;
+                    MeleeAttackArea.gameObject.SetActive(false);
+                }
+            }
         }
     }
     void HorizontalMoveLogic() 
     {
-        if (canMove)
+        if (canMove && !Dashing)
         {
             if (Input.GetButton("Horizontal"))
             {
                 if (Input.GetAxis("Horizontal") < 0)
                 {
                     Rb2D.velocity = new Vector2(-moveSpeed, Rb2D.velocity.y);
-                    spriteRenderer.flipX = true;
-                    MeleeAttackArea.transform.localScale = new Vector2(-1, 1);
-                    
+                    if (!Attacking) 
+                    { 
+                        gameObject.transform.localScale = new Vector2(-1, 1);
+                        facingRignt = false;
+                    }
                 }
                 else if (Input.GetAxis("Horizontal") > 0)
                 {
                     Rb2D.velocity = new Vector2(moveSpeed, Rb2D.velocity.y);
-                    spriteRenderer.flipX = false;
-                    MeleeAttackArea.transform.localScale = new Vector2(1, 1);
+                    if (!Attacking) 
+                    { 
+                        gameObject.transform.localScale = new Vector2(1, 1);
+                        facingRignt = true;
+                    }
                 }
             }
             else
@@ -123,43 +203,53 @@ public class Player : MonoBehaviour
     }
     void DashLogic() 
     {
+        if (CheckGround.isGrounded && !Dashing)
+        {
+            canDash = true;
+        }
         // control del cooldown del dash
         if (currentDashCooldown > 0)
         {
             currentDashCooldown -= Time.deltaTime;
         }
-        // logica al activar el dash
-        if (Input.GetKey("z") && currentDashCooldown <= 0 && !dashing && canMove) 
+        if (canMove)
         {
-            canMove = false;
-            currentDashDuration = 0;
-            dashing = true;
-            Rb2D.gravityScale = 0;
-            if (spriteRenderer.flipX)
+            // logica al activar el dash
+            if (Input.GetKey("z") && currentDashCooldown <= 0 && !Dashing && canDash && !Attacking)
             {
-                Rb2D.velocity = Vector2.left * moveSpeed * 3;
+                canDash = false;
+                currentDashDuration = 0;
+                Dashing = true;
+                Rb2D.gravityScale = 0;
+                if (!facingRignt)
+                {
+                    Rb2D.velocity = Vector2.left * moveSpeed * 3;
+                }
+                else
+                {
+                    Rb2D.velocity = Vector2.right * moveSpeed * 3;
+                }
             }
-            else 
+            // logica durante la duracion del dash
+            if (Dashing)
             {
-                Rb2D.velocity = Vector2.right * moveSpeed * 3;
+                if (currentDashDuration < dashDuration)
+                {
+                    currentDashDuration += Time.deltaTime;
+
+                }
+                // logica despues de la duracion del dash
+                else
+                {
+                    currentDashCooldown = dashCooldown;
+                    Dashing = false;
+                    Rb2D.gravityScale = 1;
+                }
             }
         }
-        // logica durante la duracion del dash
-        if (dashing)
+        else 
         {
-            if (currentDashDuration < dashDuration)
-            {
-                currentDashDuration += Time.deltaTime;
-                
-            }
-            // logica despues de la duracion del dash
-            else
-            {
-                canMove = true;
-                currentDashCooldown = dashCooldown;
-                dashing = false;
-                Rb2D.gravityScale = 1;
-            }
+            Dashing = false;
         }
     }
     void CustomGravityLogic() 
@@ -175,5 +265,11 @@ public class Player : MonoBehaviour
                 Rb2D.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
             }
         }
+    }
+    public enum BulletTipe 
+    {
+        Basic,
+        Blast,
+        Toxic
     }
 }
